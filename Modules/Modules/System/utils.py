e862369ecf5369109fdb1deccf6fc7751e279681
ShengKhai <crimsonflare_91@hotmail.com>
2015-03-09 03:10:09 +0800

    a
    a

---
+++ Modules/Modules/System/utils.py
@@ -0,0 +1,11 @@
+import json
+
+def writeJson(fileName, data):
+    with open(fileName, "w") as outfile:
+        json.dump(data, outfile)
+    file.close(outfile)
+    
+def readJson(fileName):
+    with open(fileName, "r") as infile:
+        data = (open(infile.name, "r").read())
+    return data
